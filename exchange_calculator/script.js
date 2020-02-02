0a4facf9d25a412092eb8265b3ad6a1b411dc782
Trevor Hodsdon <mrtj458@gmail.com>
2020-02-01 23:12:55 -0700

    First 4 projects

---
+++ exchange_calculator/script.js
@@ -0,0 +1,37 @@
+const currencyEl_1 = document.getElementById('currency-one')
+const currencyEl_2 = document.getElementById('currency-two')
+const amountEl_1 = document.getElementById('amount-one')
+const amountEl_2 = document.getElementById('amount-two')
+
+const rateEl = document.getElementById('rate')
+const swap = document.getElementById('swap')
+
+function calculate() {
+  console.log('called!')
+  const currency_one = currencyEl_1.value
+  const currency_two = currencyEl_2.value
+
+  fetch(`https://api.exchangerate-api.com/v4/latest/${currency_one}`)
+    .then(res => res.json())
+    .then(data => {
+      const rate = data.rates[currency_two]
+
+      rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`
+
+      amountEl_2.value = (amountEl_1.value * rate).toFixed(2)
+    })
+}
+
+currencyEl_1.addEventListener('change', calculate)
+amountEl_1.addEventListener('input', calculate)
+currencyEl_2.addEventListener('change', calculate)
+amountEl_2.addEventListener('input', calculate)
+
+swap.addEventListener('click', () => {
+  const temp = currencyEl_1.value
+  currencyEl_1.value = currencyEl_2.value
+  currencyEl_2.value = temp
+  calculate()
+})
+
+calculate()
